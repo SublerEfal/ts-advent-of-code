d9b6ce1fa7783b2e29c1dfa774f0b5998642010e
SublerEfal <sam.peter.vr@gmail.com>
2024-12-10 19:51:47 +0100

    Update how the script is started to be more convenient

---
+++ README.md
@@ -0,0 +1,4 @@
+Run code: `npm start {year} {day}`<br>
+For example: `npm start 2015 01`
+
+To run once without nodemon, use `npm run start-once {year} {day}`
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,7 @@
-import "./2024/10/solve";
+// import "./2015/13/solve";
+
+(async function () {
+	const year = process.argv[2];
+	const day = process.argv[3].padStart(2, "0");
+	await import(`./${year}/${day}/solve`);
+})();
